244b9f1baa815e1d241651a77cd02ba08fc8c306
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-18 16:14:59 +0300

    Added the reverse action

---
+++ MenuBuilder/MenuBuilder/StringMenu/ReverseAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MenuBuilder.StringMenu
+{
+    public class ReverseAction : IActions<string>
+    {
+        public string Action(params string[] parameters)
+        {
+            if(parameters.Length > 1 || parameters.Length == 0)
+            {
+                return "Invalid Input";
+            }
+            return new string(parameters[0].ToCharArray().Reverse().ToArray());
+        }
+    }
+}
--- MenuBuilder/MenuBuilder/StringMenu/StringMenu.cs
+++ MenuBuilder/MenuBuilder/StringMenu/StringMenu.cs
@@ -1,10 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MenuBuilder.StringMenu
 {
-    class StringMenu
+    public class StringMenu : BasicMenu<string>
     {
+        private readonly List<string> _inputOptions = new List<string> { "1", "2", "3", "4" };
+        private readonly List<string> _requaiersInput = new List<string> { "1", "2" };
+        private BasicValidator _validator;
+        private ConsoleDisplayer _consoleDisplayer;
+
+        private Dictionary<string, IActions<string>> _options;
+
+        private Dictionary<string, string> _displayOptions;
+
+        public StringMenu()
+        {
+            _displayOptions = new Dictionary<string, string>
+            {
+                {"1", "Add Numbers" },
+                {"2", "Reduce Numbers" },
+                {"3", "Go To String Menu" },
+                {"4", "Exit" }
+            };
+            _consoleDisplayer = new ConsoleDisplayer(_displayOptions);
+            _options = new Dictionary<string, IActions<string>>
+            {
+                {"Reverse", new ReverseAction() },
+                {"Combine", new ReduceAction() },
+                {"Exit", new ExitAction(null) }
+            };
+
+            var options = _options.Keys.ToList();
+            _validator = new IntValidator(_inputOptions);
+        }
+        public override void Main()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RunOption(string option, params string[] variables)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
